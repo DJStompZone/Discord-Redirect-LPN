35435f201272385ee43ca0916c8ac20c0481ec99
DJ Stomp <stompzone@gmail.com>
2023-12-18 21:45:34 +0000

    Update url

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 setTimeout(function() {
-  let fallbackUrl = "https://discord.gg/wYQwBwkBGg"
+  let fallbackUrl = "https://discord.gg/QaxNSMXhJf"
   window.location.href = process.env.DISCORD_INVITE_LINK ?? fallbackUrl;
 }, 5000);
